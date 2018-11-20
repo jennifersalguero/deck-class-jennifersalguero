e16433143498a38062690e5c4b93ec347c9ab63f
jennifersalguero <43127265+jennifersalguero@users.noreply.github.com>
2018-11-20 03:05:44 -0500

    Update deck-assignment.py

--- deck-assignment.py
+++ deck-assignment.py
@@ -1,16 +1,24 @@
-# Import statements go here
+# Import random
 
 
 class Deck:
     """
-    Your docstring goes here
+    52 cards in a set
     """
 
     ranks = [str(r) for r in range(2, 11)] + list('JQKA')
-    suits = list('cdhs')
+    suits = list("Clubs","Hearts","Diamonds","Spades")
 
-    def __init__(self):
-        self._cards = []  # Your code goes inside the listcomp
+    def __init__(self,rank,suit):
+        self._cards = [52]  # Your code goes inside the listcomp
+        if self.ranks == "Ace":
+            return 1
+        if self.ranks =="Joker":
+             return 11
+        if self.ranks == "Queen":
+            return 12
+        if self.ranks == "King":
+            return 13
 
     def __repr__(self):
         pass
@@ -19,4 +27,6 @@ class Deck:
         return len(self._cards)
 
     def deal(self):
-        pass
+        for suits in rank:
+          cards.append((rank,suit))
+          random.shuffle(cards)
